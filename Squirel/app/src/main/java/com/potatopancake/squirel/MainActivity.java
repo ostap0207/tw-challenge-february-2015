443f910cdf8a08f4b8d5f2a093fb9ac941270890
Ostap Maliuvanchuk <ostap_mal@hotmail.com>
2015-02-21 17:15:20 +0200

    prototype

--- Squirel/app/src/main/java/com/potatopancake/squirel/MainActivity.java
+++ Squirel/app/src/main/java/com/potatopancake/squirel/MainActivity.java
@@ -5,8 +5,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.gc.materialdesign.views.ButtonFloat;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -17,8 +22,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-        intentIntegrator.initiateScan();
+        getSupportActionBar().setElevation(0);
+        ButtonFloat btnScan = (ButtonFloat) findViewById(R.id.buttonFloat);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                new String[]{"1. Rimi store      -25 EUR", "2. Parking      -1 EUR"});
+        list.setAdapter(arrayAdapter);
+
     }
 
     @Override
--- Squirel/app/src/main/java/com/potatopancake/squirel/ReceiptActivity.java
+++ Squirel/app/src/main/java/com/potatopancake/squirel/ReceiptActivity.java
@@ -1,26 +1,114 @@
 package com.potatopancake.squirel;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.paypal.android.sdk.payments.PayPalAuthorization;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 
 public class ReceiptActivity extends ActionBarActivity {
 
+    private static String clientId = "Aeij3mlTgy6_HQXhyU32v7jqxoyoA0Bd9O9zqUJAT3YsHT-oaIrWM-65fPFO0Pa732CzzzINkV6R04b4";
+
+
     private static PayPalConfiguration config = new PayPalConfiguration()
-
-            // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
-            // or live (ENVIRONMENT_PRODUCTION)
             .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
-
-            .clientId("<YOUR_CLIENT_ID>");
+            .rememberUser(true)
+            .defaultUserEmail("omaliuvanchuk@gmail.com")
+            .sandboxUserPassword("12345678")
+            .forceDefaultsOnSandbox(true)
+            .merchantName("Squirel")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"))
+            .clientId(clientId);
+    private Button btnPay;
+    private int REQUEST_CODE_FUTURE_PAYMENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
 
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+
+
+        btnPay = (Button)findViewById(R.id.btnPay);
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFuturePaymentPressed(v);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            PayPalAuthorization auth = data
+                    .getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+            if (auth != null) {
+                String authorization_code = auth.getAuthorizationCode();
+                Intent intent = new Intent(this, SuccessActivity.class);
+                startActivity(intent);
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("FuturePaymentExample", "The user canceled.");
+        } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("FuturePaymentExample",
+                    "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    public void onFuturePaymentPressed(View pressed) {
+        Intent intent = new Intent(this, PayPalFuturePaymentActivity.class);
+
+        // send the same configuration for restart resiliency
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startActivityForResult(intent, REQUEST_CODE_FUTURE_PAYMENT);
+    }
+
+    public void onBuyPressed(View pressed) {
+
+        // PAYMENT_INTENT_SALE will cause the payment to complete immediately.
+        // Change PAYMENT_INTENT_SALE to
+        //   - PAYMENT_INTENT_AUTHORIZE to only authorize payment and capture funds later.
+        //   - PAYMENT_INTENT_ORDER to create a payment for authorization and capture
+        //     later via calls from your server.
+
+        PayPalPayment payment = new PayPalPayment(new BigDecimal("35.0"), "EUR", "PotatoPancakes",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(intent, 0);
     }
 
 
